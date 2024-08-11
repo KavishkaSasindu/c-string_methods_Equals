ddba9db32516dc12e316d3d3bb9ccdbfff96bcd1
KavishkaSasindu <sasindu0215@gmail.com>
2024-08-12 00:47:05 +0530

    initialize commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.Write("Enter your name :");
+            string name = Console.ReadLine();
+
+            if (name.Equals("Kavishka Sasindu"))
+            {
+                Console.WriteLine($"Hello {name}");
+            }
+            else
+            {
+                Console.WriteLine("You are not Kavishka Sasindu");
+            }
+
+            string message = "Hello Kavishka";
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.WriteLine(message[i]);
+            }
+        }
+    }
+}
